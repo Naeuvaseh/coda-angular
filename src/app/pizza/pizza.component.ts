7d919b377867725bf388493b48e1579ca8c69bc5
Naeuvaseh <naeuvaseh@gmail.com>
2019-11-07 14:33:53 -0500

    1 file modified

--- src/app/pizza/pizza.component.ts
+++ src/app/pizza/pizza.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PizzaComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    /*
+      #1: In /app, create 'containers' directory.
+
+      #2: In /app, create 'components' directory.
+
+      #3: Move PizzaComponent into /containers.
+
+      #4: Create 'topping' component in /components
+
+      #5: Create toppings array variable in ToppingComponent.
+
+      #6: Create topping interface to describe our toppings.
+
+      #7: Update topping array variable.
+
+      #7: In /app, create 'model's directory.
+
+      #7: Move Toppings interface into /models.
+
+      #8: Update imports.
+    */
+  }
 
   ngOnInit() {
+  
   }
 
 }
